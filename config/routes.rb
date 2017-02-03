66095aed2737b60c8182fca7564a63847c60d5a7
Faris Mohammed <farismosman@gmail.com>
2017-02-03 22:58:13 +0200

    deleted all files

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :user do
-    get "index"
-    get "create"
-  end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
